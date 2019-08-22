6abef1f7ee9de7b63da2264a6e41650b749b4eec
Milan Keser <mkeser@dowosoft.com>
2019-08-22 19:20:54 +0200

    added prisma secret

--- src/prisma.js
+++ src/prisma.js
@@ -2,13 +2,8 @@ import { Prisma } from "prisma-binding"
 
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
-  endpoint: "http://192.168.99.100:4466/"
+  endpoint: "http://192.168.99.100:4466/",
+  secret: "secretexample"
 })
 
 export default prisma
-
-// prisma.query prisma.mutation prisma.subscription prisma.exists
-
-// prisma.query.users(null, "{ id name email }").then(data => {
-//   console.log(data)
-// })
