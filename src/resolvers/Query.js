843376204bbeec2d36c2e2993467e5c74980f5bc
Milan Keser <mkeser@dowosoft.com>
2019-08-29 18:24:35 +0200

    Locking down queries

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,8 @@
+import getUserId from "../utils/getUserId";
+
 const Query = {
+  // USERS
+  //
   users (parent, args, { prisma }, info) {
     const opArgs = {}
 
@@ -15,33 +19,82 @@ const Query = {
 
     return prisma.query.users(opArgs, info)
   },
-  posts (parent, args, { prisma }, info) {
-    const opArgs = {}
+  me (parent, args, { prisma, request }, info) {
+    const userId = getUserId(request)
+
+    return prisma.query.user({
+      where: {
+        id: userId
+      }
+    })
+  },
+  // POSTS
+  //
+  myPosts (parent, args, { prisma, request }, info) {
+    const userId = getUserId(request)
+    const opArgs = {
+      where: {
+        author: {
+          id: userId
+        }
+      }
+    }
 
     if (args.query) {
-      opArgs.where = {
-        OR: [{
-          title_contains: args.query
-        },
-        {
-          body_contains: args.query
-        }]
+      opArgs.where.OR = [{
+        title_contains: args.query
+      }, {
+        body_contains: args.query
+      }]
+    }
+
+    return prisma.query.posts(opArgs, info)
+
+  },
+  posts (parent, args, { prisma }, info) {
+    const opArgs = {
+      where: {
+        published: true
       }
+    }
+
+    if (args.query) {
+      opArgs.where.OR = [{
+        title_contains: args.query
+      },
+      {
+        body_contains: args.query
+      }]
     }
 
     return prisma.query.posts(opArgs, info)
   },
+  async post (parent, args, { prisma, request }, info) {
+    const userId = getUserId(request)
+
+    const posts = await prisma.query.posts({
+      where: {
+        id: args.id,
+        OR: [{
+          published: true
+        }, {
+          author: {
+            id: userId
+          }
+        }]
+      }
+    }, info)
+
+    if (posts.length === 0) throw new Error('Post not found')
+
+    return posts[0]
+  },
+  // COMMENTS
+  //
   comments (parent, args, { prisma }, info) {
     return prisma.query.comments(null, info)
   },
-  me () {
-    return {
-      id: "1",
-      name: "Milan",
-      email: "milan@test.com",
-      age: 26
-    }
-  }
+
 }
 
 export default Query
