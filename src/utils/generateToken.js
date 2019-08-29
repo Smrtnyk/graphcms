1daf9bd9c17e9c52b1f5897f14c69e198c7651db
Milan Keser <mkeser@dowosoft.com>
2019-08-30 00:50:46 +0200

    Added jwt secret to env

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken"
 
 const generateToken = id => {
-  return jwt.sign({ userId: id }, 'somesecret', { expiresIn: "7 days" })
+  return jwt.sign({ userId: id }, process.env.JWT_SECRET, { expiresIn: "7 days" })
 }
 
 export default generateToken
